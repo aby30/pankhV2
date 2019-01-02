641f6f8ff227849193172710c413c546285a25d2
Abhipray <abhipray.shailendra3@gmail.com>
2019-01-02 19:21:02 +0530

    Finalising the content and new changes as per the client

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 function hideMenu() {
   document.getElementById('triggerMenu').checked = false;
+  $(".header__inner").toggleClass("menuInView");
+  $("body").toggleClass("noScroll");
 }
 
 function showMsgModal() {
@@ -55,9 +57,69 @@ function showMenu() {
 
 //script To change background for Header onScroll Home page START
 $(document).bind('scroll', function () {
-  if ($(document).scrollTop() > 200) {
-    $('.header__inner').addClass('bgOnScroll');
+  if (window.innerWidth < 640) {
+    if ($(document).scrollTop() > 80) {
+      $('.header__inner').addClass('bgOnScroll');
+    } else {
+      $('.header__inner').removeClass('bgOnScroll');
+    }
   } else {
-    $('.header__inner').removeClass('bgOnScroll');
+    if ($(document).scrollTop() > 150) {
+      $('.header__inner').addClass('bgOnScroll');
+    } else {
+      $('.header__inner').removeClass('bgOnScroll');
+    }
   }
+
 });
+
+
+
+// Questions display START
+
+document.addEventListener('DOMContentLoaded',function(event){
+  // array with texts to type in typewriter
+
+
+  var dataText = [ "Where to buy chic wedding dresses?", "Dreamy pre wedding shoot locations in different cities?", "List of best bridal makeup artists in the city?", "How to plan a destination wedding on a budget?", "Which are the available wedding venues in the city?", "Best wedding photographers under 25k?", "What to gift your sister-in-law for the wedding?", "What should bride’s sister wear during ring ceremony?"];
+
+  // type one text in the typwriter
+  // keeps calling itself until the text is finished
+  function typeWriter(text, i, fnCallback) {
+    // chekc if text isn't finished yet
+    if (i < (text.length)) {
+      // add next character to h1
+     document.getElementById("questions").innerHTML = text.substring(0, i+1) +'<span aria-hidden="true"></span>';
+
+      // wait for a while and call this function again for next character
+      setTimeout(function() {
+        typeWriter(text, i + 1, fnCallback)
+      }, 100);
+    }
+    // text finished, call callback if there is a callback function
+    else if (typeof fnCallback == 'function') {
+      // call callback after timeout
+      setTimeout(fnCallback, 700);
+    }
+  }
+  // start a typewriter animation for a text in the dataText array
+   function StartTextAnimation(i) {
+     if (typeof dataText[i] == 'undefined'){
+        setTimeout(function() {
+          StartTextAnimation(0);
+        }, 20000);
+     }
+     // check if dataText[i] exists
+    if (i < dataText[i].length) {
+      // text exists! start typewriter animation
+     typeWriter(dataText[i], 0, function(){
+       // after callback (and whole text has been animated), start next text
+       StartTextAnimation(i + 1);
+     });
+    }
+  }
+  // start the text animation
+  StartTextAnimation(0);
+});
+
+// Questions display END
